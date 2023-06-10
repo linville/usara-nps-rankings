0432b00aaea42df079e23623b42906e9d4b8cb2e
Aaron Linville <aaron@linville.org>
2023-06-09 22:21:07 -0400

    Parse race info and results column names of the standard USARA results spreadsheet.

--- importer.py
+++ importer.py
@@ -1,50 +1,79 @@
-import json
-import tomllib
+from openpyxl import load_workbook
 
 
 class ResultsImporter(object):
     def __init__(self, ranker):
         self._ranker = ranker
 
-    @property
-    def division_results(self):
-        return self._division_results
+        # Temporary variables set during import
+        self._race_info = {
+            "Race Name": None,
+            "Race Date": None,
+            "Race Length": None,
+            "Winning Time": None,
+            "City": None,
+            "State": None,
+        }
+
+        # Require Columns Locations
+        self._column_mapping = {
+            "Team Name": None,
+            "Division": None,
+            "Overall Place": None,
+            "Division Place": None,
+            "Racer 1": None,
+            "Racer 2": None,
+            "Racer 3": None,
+            "Racer 4": None,
+        }
 
     def import_file(self, path):
         print("Importing ", path)
 
-        if path.suffix == ".json":
-            with open(path, "r") as f:
-                self._import_race(json.load(f))
-        elif path.suffix == ".toml":
-            with open(path, "rb") as f:
-                self._import_race(tomllib.load(f))
+        if path.suffix == ".xlsx":
+            wb = load_workbook(path)
+
+            required = ["Instructions", "Race Info", "Results"]
+            for r in required:
+                if r not in wb.sheetnames:
+                    raise ValueError(f"Missing {r} worksheet: {path}")
+
+            self._import_race_info(wb.get_sheet_by_name("Race Info"))
+            self._import_results(wb.get_sheet_by_name("Results"))
         else:
             raise ValueError("Unknown file type: ", path)
 
-    def _import_race(self, race_data):
-        race_name = race_data["name"].strip()
+    def _import_race_info(self, race_info_sheet):
+        # Assume the Race Info field names are the first column and the
+        # user-provided data is the second column.
+        for cell in race_info_sheet["A"]:
+            if cell.value in self._race_info:
+                self._race_info[cell.value] = cell.offset(column=1).value
 
-        for ranking in race_data["rankings"]:
-            self._import_ranking(race_data, ranking)
+        # Verify we found the required Race Info fields.
+        if None in self._race_info.values():
+            raise ValueError("Missing some race info:", self._race_info)
 
-    def _import_ranking(self, race_data, ranking):
-        race_name = race_data["name"]
+    def _import_results(self, results_sheet):
+        # Get column indexes of required columns, this allows them to be in
+        # any order.
+        for cnum in range(1, results_sheet.max_column + 1):
+            field = results_sheet.cell(row=1, column=cnum).value
+            if field in self._column_mapping:
+                self._column_mapping[field] = cnum
 
-        division = ranking["division"]
-        team_name = ranking["team name"]
-        overall_place = ranking["overall place"]
-        division_place = ranking["division place"]
-        members = None  # ranking["members"]
+        # Verify we found the required columns.
+        if None in self._column_mapping.values():
+            raise ValueError("Missing some results columns:", self._column_mapping)
 
         points = 1
 
-        self._ranker.add_entry(
-            division,
-            team_name,
-            race_name,
-            overall_place,
-            overall_place,
-            points,
-            members,
-        )
+        # self._ranker.add_entry(
+        #    division,
+        #    team_name,
+        #    self._race_name,
+        #    overall_place,
+        #    division_place,
+        #    points,
+        #    members,
+        # )
